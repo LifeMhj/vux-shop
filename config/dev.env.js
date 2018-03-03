50b100102f4b462522f98c4c6eb81773bbc81fc6
LifeMhj <mahuajiao@live.com>
2018-03-03 14:28:01 +0800

    test

--- config/dev.env.js
+++ config/dev.env.js
@@ -2,10 +2,18 @@
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
 
-module.exports = merge(prodEnv, {
+/*module.exports = merge(prodEnv, {
     NODE_ENV: '"development"',
     BASE_API: '"http://127.0.0.1:81"',
     BASE_URL: '"http://127.0.0.1:81"',
     BASE_IMG: '"http://127.0.0.1:81/images"',
     BASE_UPLOAD: '"http://127.0.0.1:81/upload"',
+})*/
+
+module.exports = merge(prodEnv, {
+    NODE_ENV: '"development"',
+    BASE_API: '"http://a.mahuajiao.com"',
+    BASE_URL: '"http://a.mahuajiao.com"',
+    BASE_IMG: '"http://a.mahuajiao.com/images"',
+    BASE_UPLOAD: '"http://a.mahuajiao.com/upload"',
 })
